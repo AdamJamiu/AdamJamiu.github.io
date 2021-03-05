c67f6b013ed7513c021e448ec8430489950da18e
Adam Jamiu <adamjamiu98@gmail.com>
2021-03-05 09:04:35 +0100

    slider-container

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,20 +1,19 @@
 
-
-
 var line1 = document.getElementById('line1');
 var line2 = document.getElementById('line2');
 var line3 = document.getElementById('line3');
-var slider = document.getElementById('slider');
+var slider = document.getElementsByClassName("slider-container");
 
 line1.onclick = function() {
   slider.style.transform = 'translateX(0)';
 }
 
 line2.onclick = function() {
-  slider.style.transform = 'translateX(-215%)';
+  slider.style.transform = 'translateX(-25%)';
 }
 
 line3.onclick = function() {
-  slider.style.transform = 'translateX(-50)';
+  slider.style.transform = 'translateX(-50%)';
 }
+
 
