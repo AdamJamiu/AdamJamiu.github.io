465410005e090c9a2dc7d8124851963534d0ffcc
Adam Jamiu <adamjamiu98@gmail.com>
2021-03-11 13:21:03 +0100

    javascript

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,19 +1,5 @@
+let offLine= navigator.offLine;
+    alert (status);
 
-var line1 = document.getElementById('line1');
-var line2 = document.getElementById('line2');
-var line3 = document.getElementById('line3');
-var slider = document.getElementsByClassName("slider-container");
-
-line1.onclick = function() {
-  slider.style.transform = 'translateX(0)';
-}
-
-line2.onclick = function() {
-  slider.style.transform = 'translateX(-25%)';
-}
-
-line3.onclick = function() {
-  slider.style.transform = 'translateX(-50%)';
-}
-
-
+    let oNline = navigator.onLine;
+    alert (status);
